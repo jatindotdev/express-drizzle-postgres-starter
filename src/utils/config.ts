90de9cff924f9a8ba41a7de8a0f35924e1a444c9
Jatin Kumar <main.jatink@gmail.com>
2024-01-01 20:44:45 +0530

    chore: update configuration variables
    * use process.env to get variables
    * use zod to parse before we use them
    * get type completion with `env.d.ts`

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -8,7 +8,13 @@ const configSchema = z.object({
     .optional()
     .default('3000'),
   API_BASE_URL: z.string().url().default('/api'),
-  DB_URL: z.string().url().startsWith('postgres://'),
+  DB_URL: z
+    .string()
+    .url()
+    .refine(
+      url => url.startsWith('postgres://') || url.startsWith('postgresql://'),
+      'DB_URL must be a valid postgresql url'
+    ),
   FROM_NAME: z.string().default('Verify'),
   FROM_EMAIL: z.string().email(),
   AWS_ACCESS_KEY: z.string(),
@@ -17,14 +23,6 @@ const configSchema = z.object({
   JWT_SECRET: z.string(),
 });
 
-export const {
-  PORT,
-  API_BASE_URL,
-  DB_URL,
-  FROM_NAME,
-  FROM_EMAIL,
-  AWS_ACCESS_KEY,
-  AWS_SECRET_ACCESS_KEY,
-  AWS_REGION,
-  JWT_SECRET,
-} = configSchema.parse(process.env);
+configSchema.parse(process.env);
+
+export type Env = z.infer<typeof configSchema>;
--- src/utils/db.ts
+++ src/utils/db.ts
@@ -1,6 +1,5 @@
 import { drizzle } from 'drizzle-orm/postgres-js';
 import postgres from 'postgres';
-import { DB_URL } from './config';
 
-export const connection = postgres(DB_URL);
+export const connection = postgres(process.env.DB_URL);
 export const db = drizzle(connection);
