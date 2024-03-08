9fd2a97203f62a754f1fb63851f2c4b713cd6016
Jatin <main.jatink@gmail.com>
2024-03-08 21:44:31 +0530

    feat: update handler with new type logic

--- src/routes/user-routes.ts
+++ src/routes/user-routes.ts
@@ -7,26 +7,14 @@ import {
   handleVerifyUser,
 } from '@/controllers/user-controllers';
 import { authenticate } from '@/middlewares/auth';
-import { validateRequest } from '@/middlewares/validator';
-import {
-  addUserSchema,
-  deleteUserSchema,
-  loginSchema,
-  verifyUserSchema,
-} from '@/schema/user';
 import { createRouter } from '@/utils/create';
 import { Router } from 'express';
 
 export default createRouter((router: Router) => {
   router.get('/', authenticate(), handleGetUser);
-  router.get('/verify', validateRequest('query', verifyUserSchema), handleVerifyUser);
-  router.post('/create', validateRequest('body', addUserSchema), handleAddUser);
-  router.post('/login', validateRequest('body', loginSchema), handleUserLogin);
-  router.post(
-    '/remove',
-    authenticate(),
-    validateRequest('body', deleteUserSchema),
-    handleDeleteUser
-  );
+  router.get('/verify', (req, res, next) => handleVerifyUser(req, res, next));
+  router.post('/create', handleAddUser);
+  router.post('/login', handleUserLogin);
+  router.post('/remove', authenticate(), handleDeleteUser);
   router.put('/update', authenticate(), handleUpdateUser);
 });
--- src/utils/create.ts
+++ src/utils/create.ts
@@ -1,4 +1,5 @@
 import { Router, type NextFunction, type Request, type Response } from 'express';
+import { z } from 'zod';
 
 export const createRouter = (callback: (router: Router) => void) => {
   const router = Router();
@@ -6,22 +7,45 @@ export const createRouter = (callback: (router: Router) => void) => {
   return router;
 };
 
-export const createHandler = (
-  callback: ({
-    req,
-    res,
-    next,
-  }: {
-    req: Request;
-    res: Response;
-    next: NextFunction;
-  }) => Promise<void> | void
-) => {
+export function createHandler<T extends z.ZodType>(
+  schema: T,
+  handler: (
+    req: Omit<Request, keyof z.output<T>> & z.output<T>,
+    res: Response,
+    next: NextFunction
+  ) => void | Promise<void>
+): (req: Request, res: Response, next: NextFunction) => Promise<void>;
+
+export function createHandler(
+  handler: (req: Request, res: Response, next: NextFunction) => void | Promise<void>
+): (req: Request, res: Response, next: NextFunction) => Promise<void>;
+
+export function createHandler<T extends z.ZodType>(
+  schemaOrHandler:
+    | T
+    | ((req: Request, res: Response, next: NextFunction) => void | Promise<void>),
+  handler?: (
+    req: Omit<Request, keyof z.output<T>> & z.output<T>,
+    res: Response,
+    next: NextFunction
+  ) => void | Promise<void>
+) {
   return async (req: Request, res: Response, next: NextFunction) => {
     try {
-      await callback({ req, res, next });
-    } catch (err) {
-      next(err);
+      if (handler) {
+        const schema = schemaOrHandler as T;
+        schema.parse(req);
+        await handler(req, res, next);
+      } else {
+        const handler = schemaOrHandler as (
+          req: Request,
+          res: Response,
+          next: NextFunction
+        ) => void | Promise<void>;
+        await handler(req, res, next);
+      }
+    } catch (error) {
+      next(error);
     }
   };
-};
+}
